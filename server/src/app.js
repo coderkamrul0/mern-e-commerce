dec5117b5c1637777df8f999b6cd50ffdcc3649e
Kamrul Hasan <kamrul444216@gmail.com>
2023-10-22 02:06:26 +0600

    Project setup

---
+++ server/src/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const createError = require("http-errors");
+const xssClean = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const userRouter = require("./routers/userRouter");
+
+const app = express();
+
+
+const rateLimiter = rateLimit({
+    windowMs: 1* 60 * 1000, // 1 minute
+    max: 100,
+    message: 'Request limit cross. Please try again later'
+})
+
+app.use(rateLimiter);
+app.use(xssClean());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use('/api/users', userRouter);
+
+app.get("/test", (req, res) => {
+  res.status(200).send({
+    message: "api is working fine now",
+  });
+});
+
+
+
+
+// client error handling
+app.use((req, res, next) => {
+  next(createError(404, "route not found"));
+});
+// server error handling
+app.use((err, req, res, next) => {
+  return res
+    .status(err.status || 500)
+    .json({ success: "false", message: err.message });
+});
+
+module.exports = app;
---
+++ server/src/models/userModel.js
@@ -0,0 +1,8 @@
+const users = [
+  { id: 1, name: "kamrul" },
+  { id: 2, name: "hasan" },
+  { id: 3, name: "jsdude" },
+  { id: 4, name: "namnai" },
+];
+
+module.exports = { users };
