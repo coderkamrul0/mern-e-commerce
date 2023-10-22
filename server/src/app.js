6706d68f5e86fb13522b8a0bf3f832d90a343b5a
Kamrul Hasan <kamrul444216@gmail.com>
2023-10-22 15:12:24 +0600

    response controller

--- server/src/app.js
+++ server/src/app.js
@@ -6,15 +6,15 @@ const xssClean = require("xss-clean");
 const rateLimit = require("express-rate-limit");
 const userRouter = require("./routers/userRouter");
 const seedRouter = require("./routers/seedRouter");
+const { errorResponse } = require("./controllers/responseController");
 
 const app = express();
 
-
 const rateLimiter = rateLimit({
-    windowMs: 1* 60 * 1000, // 1 minute
-    max: 100,
-    message: 'Request limit cross. Please try again later'
-})
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 100,
+  message: "Request limit cross. Please try again later",
+});
 
 app.use(rateLimiter);
 app.use(xssClean());
@@ -22,9 +22,8 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.use('/api/users', userRouter);
-app.use('/api/seed', seedRouter);
+app.use("/api/users", userRouter);
+app.use("/api/seed", seedRouter);
 
 app.get("/test", (req, res) => {
   res.status(200).send({
@@ -32,18 +31,13 @@ app.get("/test", (req, res) => {
   });
 });
 
-
-
-
 // client error handling
 app.use((req, res, next) => {
   next(createError(404, "route not found"));
 });
 // server error handling
 app.use((err, req, res, next) => {
-  return res
-    .status(err.status || 500)
-    .json({ success: "false", message: err.message });
+  return errorResponse(res, { statusCode: err.status, message: err.message });
 });
 
 module.exports = app;
--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,11 +1,12 @@
 const createError = require("http-errors");
 const User = require("../models/userModel");
+const { successResponse } = require("./responseController");
 
 const getUsers = async (req, res, next) => {
   try {
     const search = req.query.search || "";
     const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 2;
+    const limit = Number(req.query.limit) || 1;
 
     const searchRegEx = new RegExp(".*" + search + ".*", "i");
     const filter = {
@@ -26,14 +27,17 @@ const getUsers = async (req, res, next) => {
 
     if (!users) throw createError(404, "user not found.");
 
-    res.status(200).send({
-      message: "All users here",
-      users,
-      pagination: {
-        totalPages: Math.ceil(count / limit),
-        currentPage: page,
-        previousPage: page - 1 > 0 ? page - 1 : null,
-        nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null,
+    return successResponse(res, {
+      statusCode: 200,
+      message: "return success",
+      payload: {
+        users,
+        pagination: {
+          totalPages: Math.ceil(count / limit),
+          currentPage: page,
+          previousPage: page - 1 > 0 ? page - 1 : null,
+          nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null,
+        },
       },
     });
   } catch (error) {
